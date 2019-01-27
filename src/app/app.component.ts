80dd2fa269a213316b174c4636bb9723a5fd29ef
imalexianne <imanirax@yahoo.fr>
2019-01-27 18:43:48 +0200

    create a class quote and quot component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  // title:"Quotes"
+//  quotes:string[];
+//  constructor(){
+//    this.quotes=["an","Buy"];
+// }
+ 
 }
